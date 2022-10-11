109597c8ff501059f9c52beb6d4818671af139cd
Yoann Collot <yoann.collot@adeo.com>
2022-10-11 16:04:47 +0200

    add descrpition

--- model/missions.js
+++ model/missions.js
@@ -2,5 +2,6 @@ var mongoose = require('mongoose');
 var missionSchema = new mongoose.Schema({
   name: String,
   company: String,
+  description: String,
 });
 mongoose.model('Mission', missionSchema);
